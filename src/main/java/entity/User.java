ddc4375c2a5c6f41a048db12a1cd3539d06580ce
lucifer7 <skyvoice@foxmail.com>
2016-06-12 17:06:27 +0800

    add test entity

---
+++ src/main/java/entity/User.java
@@ -0,0 +1,17 @@
+package entity;
+
+import lombok.Data;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Jingyi.Yang
+ * Date: 2016/6/12
+ * Time: 17:05
+ **/
+@Data
+public class User {
+    private String name;
+    private Integer age;
+    private String nickName;
+
+}
