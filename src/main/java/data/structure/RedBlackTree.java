ad05c42601e1d95861e7a901144badba35f3be19
lucifer7 <skyvoice@foxmail.com>
2016-06-15 16:29:20 +0800

    tree

---
+++ src/main/java/data/structure/RedBlackTree.java
@@ -0,0 +1,19 @@
+package data.structure;
+
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Jingyi.Yang
+ * Date: 2016/6/15
+ * Time: 15:44
+ **/
+public class RedBlackTree {
+    private static Map nodes;
+    private Node root;
+
+    private static class Node {
+
+    }
+
+}
