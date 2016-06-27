6ed81cad5e36e78481649cd1ccb8dfd523e994d0
lucifer7 <skyvoice@foxmail.com>
2016-06-27 21:37:03 +0800

    cancel and jcip dependency

---
+++ src/main/java/concurrency/cancel/PrimeGenerator.java
@@ -0,0 +1,38 @@
+package concurrency.cancel;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lucifer on 2016-6-27.
+ * 使用 volatile 类型的域来保存取消状态
+ */
+@ThreadSafe
+public class PrimeGenerator implements Runnable {
+    @GuardedBy("this")
+    private final List<BigInteger> primes = new ArrayList<>();
+    private volatile boolean cancelled;
+
+    @Override
+    public void run() {
+        BigInteger p = BigInteger.ONE;
+        while (!cancelled) {
+            p = p.nextProbablePrime();
+            synchronized (this) {
+                primes.add(p);
+            }
+        }
+    }
+
+    public void cancel() {
+        cancelled = true;
+    }
+
+    public synchronized List<BigInteger> get() {
+        return new ArrayList<>(primes);
+    }
+}
