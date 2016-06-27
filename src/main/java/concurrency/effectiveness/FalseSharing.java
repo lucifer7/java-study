65c520f427b46376e6170d4be6aaf31a0017de1a
lucifer7 <skyvoice@foxmail.com>
2016-06-27 17:34:27 +0800

    false sharing and cache miss and cache line

---
+++ src/main/java/concurrency/effectiveness/FalseSharing.java
@@ -0,0 +1,75 @@
+package concurrency.effectiveness;
+
+/**
+ * <B>系统名称：</B><BR>
+ * <B>模块名称：</B><BR>
+ * <B>中文类名：</B><BR>
+ * <B>概要说明：</B><BR>
+ *
+ * @author carl.yu
+ * @since 2016/6/27
+ */
+public class FalseSharing implements Runnable {
+    public final static int NUM_THREADS = 4; // change
+    public final static long ITERATIONS = 500L * 1000L * 100L;
+    private final int arrayIndex;
+
+    private static VolatileLong[] longs = new VolatileLong[NUM_THREADS];
+    static
+    {
+        for (int i = 0; i < longs.length; i++)
+        {
+            longs[i] = new VolatileLong();
+        }
+    }
+
+    public FalseSharing(final int arrayIndex)
+    {
+        this.arrayIndex = arrayIndex;
+    }
+
+    public static void main(final String[] args) throws Exception
+    {
+        final long start = System.nanoTime();
+        runTest();
+        System.out.println("duration = " + (System.nanoTime() - start));
+        //duration = 682356933
+        //duration = 2824266320
+        //duration = 2959156374
+    }
+
+    private static void runTest() throws InterruptedException
+    {
+        Thread[] threads = new Thread[NUM_THREADS];
+        for (int i = 0; i < threads.length; i++)
+        {
+            threads[i] = new Thread(new FalseSharing(i));
+        }
+
+        for (Thread t : threads)
+        {
+            t.start();
+        }
+
+        for (Thread t : threads)
+        {
+            t.join();
+        }
+    }
+
+    public void run()
+    {
+        long i = ITERATIONS + 1;
+        while (0 != --i)
+        {
+            longs[arrayIndex].value = i;
+        }
+    }
+
+    public final static class VolatileLong
+    {
+        public volatile long value = 0L;
+        //public long p1, p2, p3, p4, p5, p6; // comment out
+    }
+
+}
---
+++ src/main/java/concurrency/effectiveness/L1CacheMiss.java
@@ -0,0 +1,47 @@
+package concurrency.effectiveness;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Jingyi.Yang
+ * Date: 2016/6/27
+ * Time: 17:09
+ **/
+public class L1CacheMiss {
+    private static final int RUNS = 10;
+    private static final int DIMENSION_1 = 1024 * 1024;
+    private static final int DIMENSION_2 = 62;
+
+    private static long[][] longs;
+
+    public static void main(String[] args) throws Exception {
+        Thread.sleep(10000);
+        longs = new long[DIMENSION_1][];
+        for (int i = 0; i < DIMENSION_1; i++) {
+            longs[i] = new long[DIMENSION_2];
+            for (int j = 0; j < DIMENSION_2; j++) {
+                longs[i][j] = 0L;
+            }
+        }
+        System.out.println("starting....");
+
+        final long start = System.nanoTime();
+        long sum = 0L;
+        for (int r = 0; r < RUNS; r++) {
+
+            for (int j = 0; j < DIMENSION_2; j++) {
+                for (int i = 0; i < DIMENSION_1; i++) {
+                    sum += longs[i][j];
+                }
+            }   //l1 cache miss
+
+           /* for (int i = 0; i < DIMENSION_1; i++) {
+                for (int j = 0; j < DIMENSION_2; j++) {
+                    sum += longs[i][j];
+                }   //use L1 cache
+            }*/
+        }
+        System.out.println("duration = " + (System.nanoTime() - start));
+    }
+}
+//duration = 721778281
+//duration = 9963013912
