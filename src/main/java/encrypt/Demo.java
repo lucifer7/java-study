d522a4fbcf4237f0428d8e28dc37924d49ec3d63
lucifer7 <skyvoice@foxmail.com>
2016-08-10 17:24:13 +0800

    hex, encoder

---
+++ src/main/java/encrypt/Demo.java
@@ -0,0 +1,60 @@
+package encrypt;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+import java.nio.charset.Charset;
+import java.security.MessageDigest;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Jingyi.Yang
+ * Date: 2016/7/28
+ * Time: 14:19
+ **/
+public class Demo {
+    /**
+     * BASE64解密
+     *
+     * @param key
+     * @return
+     * @throws Exception
+     */
+    public static byte[] decryptBASE64(String key) throws Exception {
+        return (new BASE64Decoder()).decodeBuffer(key);
+    }
+
+    /**
+     * BASE64加密
+     *
+     * @param key
+     * @return
+     * @throws Exception
+     */
+    public static String encryptBASE64(byte[] key) throws Exception {
+        return (new BASE64Encoder()).encodeBuffer(key);
+    }
+    /**
+     * MD5加密
+     *
+     * @param data
+     * @return
+     * @throws Exception
+     */
+    public static byte[] encryptMD5(byte[] data) throws Exception {
+
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
+        md5.update(data);
+
+        return md5.digest();
+
+    }
+
+
+
+    public static void main(String[] args) throws Exception{
+
+    }
+
+
+}
---
+++ src/main/java/jvm/SubListGc.java
@@ -0,0 +1,39 @@
+package jvm;
+
+import com.google.common.collect.Lists;
+import entity.User;
+
+import java.lang.ref.SoftReference;
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Jingyi.Yang
+ * Date: 2016/7/28
+ * Time: 10:10
+ **/
+public class SubListGc {
+
+    private static final int ARRAY_SIZE = 15000;
+
+    public static void main(String[] args) throws InterruptedException {
+        List<User> backList = Lists.newArrayListWithCapacity(ARRAY_SIZE);
+        for (int i = 0; i < ARRAY_SIZE; i++) {
+            backList.add(new User());
+        }
+        //System.out.println(backList.size());
+        //List<User> subList = backList.subList(20, 30);
+        List<User> subList = Lists.newArrayList();
+        //SoftReference softReference = new SoftReference();
+        //WeakReference weakReference = new WeakReference(backList);
+        //System.gc();
+        Runtime.getRuntime().gc();
+        TimeUnit.SECONDS.sleep(10L);
+        //System.out.println(subList);
+        //System.out.println(backList);
+        //System.out.println(weakReference);
+    }
+}
