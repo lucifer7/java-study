68a3c1b491431177f6ab3f49055644a151deed4e
lucifer7 <skyvoice@foxmail.com>
2016-08-18 20:21:24 +0800

    defects of generic type, and how to instantiate generic type with factory

--- src/main/java/data/type/generalization/EraseDefect.java
+++ src/main/java/data/type/generalization/EraseDefect.java
@@ -1,11 +1,58 @@
 package data.type.generalization;
 
+import lombok.extern.log4j.Log4j;
+
 /**
- * Usage: 泛型擦除的负面影响
+ * Usage: 泛型擦除的负面影响, 合适的泛型使用
  * Created by Lucifer
  * Date: 2016-8-15
  * Device: Aurora R5
  */
 public class EraseDefect {
+    public static void main(String[] args) {
+        HasF hasf = new HasF();
+        Manipulator<HasF> hasFManipulator = new Manipulator<>(hasf);
+        hasFManipulator.manipulate();
 
+        ManipulatorAdjusted<HasF> manipulatorAdjusted = new ManipulatorAdjusted<>(hasf);
+        manipulatorAdjusted.manipulate();
+    }
+}
+
+@Log4j
+class HasF {
+    public void f() {
+        log.info("HasF.f() is running.");
+    }
+}
+
+/* 不正解的泛型使用 */
+class Manipulator<T> {
+    private T obj;
+
+    public Manipulator(T obj) {
+        this.obj = obj;
+    }
+
+    public void manipulate() {
+        /* Invalid method invoke */
+        //obj.f();    //找不到符号
+    }
+}
+
+/* 修正后的泛型使用，给泛型一个边界 */
+class ManipulatorAdjusted<T extends HasF> {
+    private T obj;
+
+    public ManipulatorAdjusted(T obj) {
+        this.obj = obj;
+    }
+
+    public void manipulate() {
+        obj.f();
+    }
+
+    public T produce() {
+        return obj;
+    }
 }
---
+++ src/main/java/data/type/generalization/GenericNewInstance.java
@@ -0,0 +1,68 @@
+package data.type.generalization;
+
+import lombok.extern.log4j.Log4j;
+
+import java.util.Random;
+
+/**
+ * Usage: <b>泛型无法直接创建对象</b>
+ * 解决方法：
+ * 1. 传递工厂对象，通过其创建新实例
+ * 2. 使用模板设计模式
+ *
+ * @author lucifer
+ * @date 2016-8-18
+ * @devide Yoga Pro
+ */
+public class GenericNewInstance {
+    public static void main(String[] args) {
+        new Foo<Integer>(new IntegerFactory()).info();
+        new Foo<Widget>(new Widget.FactoryInner()).info();
+    }
+}
+
+class InvalidOPs<T> {
+    private final int SIZE = 100;
+
+    /* 对于泛型，任何在运行期需要知道确切类型的代码都不能工作 */
+    public void f(Object arg) {
+        /*if (arg instanceof T);      // class or array expected
+        T var = new T();            // T cannot be instantiated directly
+        T[] array = new T[];        // Array initializer expected*/
+        T[] array0 = (T[]) new Object[SIZE];    // Unchecked cast
+
+    }
+}
+
+interface Factory<T> {
+    T create();
+}
+
+@Log4j
+class Foo<T> {
+    private T x;
+
+    public <F extends Factory<T>> Foo(F factory) {
+        x= factory.create();
+    }
+
+    public void info() {
+        log.info(x.toString());
+    }
+}
+
+class IntegerFactory implements Factory<Integer> {
+    @Override
+    public Integer create() {
+        return new Random().nextInt();
+    }
+}
+
+class Widget {
+    public static class FactoryInner implements Factory<Widget> {
+        @Override
+        public Widget create() {
+            return new Widget();
+        }
+    }
+}
