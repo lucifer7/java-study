dfcfc9a046e4509b4660309bec51eab4f45a6d95
lucifer7 <skyvoice@foxmail.com>
2016-08-21 12:29:37 +0800

    generic array item

--- src/main/java/data/type/generalization/Array/GenericArray.java
+++ src/main/java/data/type/generalization/Array/GenericArray.java
@@ -1,11 +1,34 @@
 package data.type.generalization.Array;
 
+import lombok.extern.log4j.Log4j;
+
 /**
- * Usage: <b> </b>
+ * Usage: <b>泛型数组无法直接创建，但可以定义引用</b>
+ * <b>成功创建泛型数组的唯一方式：创建一个类型擦除的数组，然后转型</b>
  *
  * @author lucifer
  * @date 2016-8-19
  * @devide Yoga Pro
  */
+@Log4j
 public class GenericArray {
+    public static final int SIZE = 100;
+    private static Generic<Integer>[] gia;
+
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) {
+        //gia = (Generic<Integer>[]) new Object[SIZE];        // Cause java.lang.ClassCastException
+        gia = (Generic<Integer>[]) new Generic[SIZE];
+        log.info(gia.getClass().getSimpleName());               // Generic[], Runtime type is raw type(erased)
+
+        gia[0] = new Generic<Integer>();
+        log.info(gia[0].getClass().getSimpleName());        // Generic
+        Generic<Integer> g = gia[0];
+        log.info(g.getClass().getSimpleName());             // Generic
+
+        /*for (int i = 0; i < SIZE; i++) {
+            gia[i] = new Generic<>();
+            log.info(gia[i].toString());
+        }*/
+    }
 }
---
+++ src/main/java/data/type/generalization/Array/GenericArrayItem.java
@@ -0,0 +1,36 @@
+package data.type.generalization.Array;
+
+/**
+ * Usage: <b>数组元素是泛型参数</b>
+ *
+ * @author lucifer
+ * @date 2016-8-20
+ * @devide Yoga Pro
+ */
+public class GenericArrayItem<T> {
+    private T[] array;
+
+    public GenericArrayItem(int size) {
+        this.array = (T[]) new Object[size];        // 创建泛型数组
+    }
+
+    public void put(int index, T item) {
+        array[index] = item;
+    }
+
+    public T get(int index) {
+        return array[index];
+    }
+
+    public T[] rep() {
+        return array;
+    }
+
+    public static void main(String[] args) {
+        GenericArrayItem<Integer> gai = new GenericArrayItem<Integer>(10);
+        //Integer[] ia = gai.rep();           // [Ljava.lang.Object; cannot be cast to [Ljava.lang.Integer;
+        gai.put(0, new Integer(10));
+        Object[] oa = gai.rep();
+        System.out.println(oa[0]);
+    }
+}
