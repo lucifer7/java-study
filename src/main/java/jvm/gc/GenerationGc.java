637dd09a04b81a2166c6f22f6f864e3ef4c1ddf5
lucifer <skyvoice@foxmail.com>
2016-10-26 23:25:44 +0800

    jvm: handle promotion fail

--- src/main/java/jvm/gc/GenerationGc.java
+++ src/main/java/jvm/gc/GenerationGc.java
@@ -14,6 +14,10 @@ import static common.ProjectContants._1MB;
  * Failed on java 8
  * VM args: -XX:+PrintGCDetails -Xms20M -Xmx20M -Xmn10M -XX:SurvivorRatio=8 -XX:MaxTenuringThreshold=15
  *
+ *  是否允许担保失败  Failed on java 8
+ * VM args: -XX:+PrintGCDetails -Xms20M -Xmx20M -Xmn10M -XX:SurvivorRatio=8
+ * // Unrecognized VM option 'HandlePromotionFailure'
+ *
  * @author lucifer
  *         Date 2016-10-25
  *         Device Aurora R5
@@ -50,6 +54,23 @@ public class GenerationGc {
         allocation3 = new byte[4 * _1MB];
     }
 
+    // 4. 老年代分配担保，handle promotion failure
+    @SuppressWarnings("unused")
+    public static void testHandlePromotion() {
+        byte[] allocation1, allocation2, allocation3, allocation4, allocation5, allocation6, allocation7;
+        allocation1 = new byte[2 * _1MB];
+        allocation2 = new byte[2 * _1MB];
+        allocation2 = new byte[2 * _1MB];
+        allocation1 = null;
+        allocation4 = new byte[2 * _1MB];
+        allocation5 = new byte[2 * _1MB];
+        allocation6 = new byte[2 * _1MB];
+        allocation4 = null;
+        allocation5 = null;
+        allocation6 = null;
+        allocation7 = new byte[2 * _1MB];
+    }
+
     public static void main(String[] args) {
         // 1. Minor for young
         //testMinorGc();
@@ -58,6 +79,9 @@ public class GenerationGc {
         //testPretenureSizeThreshold();
 
         // 3. long live objects into tenure
-        testTenuringThreshold();
+        // testTenuringThreshold();
+
+        // 4. Handle promotion
+        testHandlePromotion();
     }
 }
--- src/main/java/jvm/gc/package-info.java
+++ src/main/java/jvm/gc/package-info.java
@@ -1,7 +1,7 @@
 /**
  * Usage: <b> Garbage collect, algorithm and collector config </b>
  * 1. 对象在GC时的自救, finalize() {@link jvm.gc.FinalizeEscapeGc}
- * 2. GC and objects in Yong and Tenure Generation {@link jvm.gc.GenerationGc}
+ * 2. GC and objects in Yong and Tenure Generation，分配担保 {@link jvm.gc.GenerationGc}
  *
  * @author lucifer
  * Date 2016-10-25
