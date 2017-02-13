c8baa9bbfeed8c1c7c80c37f6153c0807c00cb5e
Lucifer <skyvoice@foxmail.com>
2017-02-13 21:48:54 +0800

    Latent typing: adapter

--- src/main/java/data/type/generalization/latentTyping/AdapterLatent.java
+++ src/main/java/data/type/generalization/latentTyping/AdapterLatent.java
@@ -1,8 +1,15 @@
 package data.type.generalization.latentTyping;
 
+import generics.SimpleQueue;
+import generics.coffee.Coffee;
+import generics.coffee.Latte;
+import generics.coffee.Mocha;
+import lombok.extern.log4j.Log4j;
 import net.mindview.util.Generator;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * Usage: <b> </b>
@@ -11,12 +18,33 @@ import java.util.Collection;
  *         Date 2017-2-8
  *         Device Aurora R5
  */
+@Log4j
 public class AdapterLatent {
+    public static void main(String[] args) {
+        // 1. Adapt a Collection
+        List<Coffee> carrier = new ArrayList<>();
+        Fill.fill(new AddableCollectionAdapter<>(carrier), Coffee.class, 3);
+
+        // 2. Helper method captures the type
+        Fill.fill(Adapter.collectionAdapter(carrier), Latte.class, 2);
+        for (Coffee c : carrier) {
+            log.info(c);
+        }
+
+        // 3. Use an adapted class
+        AddableSimpleQueue<Coffee> coffeeQueue = new AddableSimpleQueue<>();
+        Fill.fill(coffeeQueue, Mocha.class, 4);
+        Fill.fill(coffeeQueue, Latte.class,2);
+        for (Coffee c : coffeeQueue) {
+            log.info(c);
+        }
+    }
 }
 
 interface Addable<T> { void add(T t); }
 
-class Filee {
+class Fill {
+    // Fill with Class token
     static <T> void fill(Addable<T> addable, Class<? extends T> classToken, int size) {
         for (int i = 0; i < size; i++) {
             try {
@@ -29,6 +57,7 @@ class Filee {
         }
     }
 
+    // Fill with Generator
     static <T> void fill(Addable<T> addable, Generator<T> generator, int size) {
         for (int i = 0; i < size; i++) {
             addable.add(generator.next());
@@ -36,6 +65,7 @@ class Filee {
     }
 }
 
+// Make any Collection addable using composition
 class AddableCollectionAdapter<T> implements Addable<T> {
     private Collection<T> c;
 
@@ -49,8 +79,17 @@ class AddableCollectionAdapter<T> implements Addable<T> {
     }
 }
 
+// A helper to capture the type automatically
 class Adapter {
     static <T> Addable<T> collectionAdapter(Collection<T> c) {
         return new AddableCollectionAdapter<T>(c);
+    }
+}
+
+// Make a SimpleQueue addable using inheritance
+class AddableSimpleQueue<T> extends SimpleQueue<T> implements Addable<T> {
+    @Override
+    public void add(T t) {
+        super.add(t);
     }
 }
