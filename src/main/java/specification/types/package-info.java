46728a99521ba7e530f1dd669834d85d270f0ce4
Lucifer <skyvoice@foxmail.com>
2017-02-28 17:56:19 +0800

    [JLS] floating type operation

---
+++ src/main/java/specification/types/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Usage: <b> JLS - Chap 4 Types, values, and variables </b>
+ * 1. integer operation: overflow and arithmetic exception {@link specification.types.IntegerOperationMain}
+ * 2. Floating-point type: zero, NaN and infinity {@link specification.types.FloatingPointTypeMain}
+ * 3. Floating type overflow, underflow, NaN, inexact results of rounding, round towards zero {@link specification.types.FloatingPointOperationMain}
+ *
+ * @author Jingyi.Yang
+ * Date 2017/2/28
+ **/
+package specification.types;
