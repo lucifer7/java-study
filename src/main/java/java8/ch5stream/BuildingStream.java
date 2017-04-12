5a0f2974ec44d154930b404b23473c419a4b0236
Lucifer <skyvoice@foxmail.com>
2017-04-12 18:22:30 +0800

    [Java8] ways to build streams

---
+++ src/main/java/java8/ch5stream/BuildingStream.java
@@ -0,0 +1,39 @@
+package java8.ch5stream;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * Usage: <b> </b>
+ *
+ * @author Jingyi.Yang
+ *         Date 2017/4/12
+ **/
+public class BuildingStream {
+    public static void main(String[] args) {
+        // 1. Stream.of
+        Stream<String> names = Stream.of("dou", "ni", "wan");
+        names.forEach(System.out::println);
+
+        // 2. Empty stream
+        Stream<Integer> empty = Stream.empty();
+        System.out.println(empty.count());
+
+        // 3. Array to Stream (primitive types have their correspondent stream, to avoid boxing)
+        int[] intArray = {2, 3, 6, 8, 9};
+        IntStream intStream = Arrays.stream(intArray);
+        System.out.println(intStream.sum());
+
+        // 4.1 Stream.iterate
+        Stream.iterate(1, i -> i + 2)
+                .limit(3)
+                .forEach(System.out::println);
+
+        // 4.2 fibonacci with iterate
+        Stream.iterate(new int[]{0, 1}, tuple -> new int[] {tuple[0] + tuple[1], tuple[0] + tuple[1] + tuple[1]})
+                .limit(4)
+                //.map(tuple -> tuple[0])
+                .forEach(tuple -> System.out.println(Arrays.toString(tuple)));
+    }
+}
---
+++ src/main/java/java8/ch5stream/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Usage: <b> Creation and Manipulation of Streams </b>
+ *
+ * @author Jingyi.Yang
+ * Date 2017/4/12
+ **/
+package java8.ch5stream;
